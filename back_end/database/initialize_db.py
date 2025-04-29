50b0705529062251ecef7deae6f74552fc6a3a4c
GUEGUEN <GUEGUEN@MacBook-Pro.local>
2025-04-29 13:46:04 +0200

    correct bug for render 3

--- back_end/database/initialize_db.py
+++ back_end/database/initialize_db.py
@@ -1,8 +1,12 @@
 # back_end/database/initialize_db.py
 
 from sqlalchemy import MetaData
+"""
 from back_end.database.connect import DatabaseConnector
 from back_end.database.repository.user_repository import UserRepository
+"""
+from connect import DatabaseConnector
+from database.repository.user_repository import UserRepository
 
 def reset_schema(connector: DatabaseConnector):
     """Supprime toutes les tables existantes."""
