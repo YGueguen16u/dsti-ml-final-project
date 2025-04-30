7a60b5d051ed8509143fd33539d032ec08decd3b
GUEGUEN <GUEGUEN@MacBook-Pro.local>
2025-04-30 22:08:25 +0200

    correct bug for vercel 22

--- back_end/config/settings.py
+++ back_end/config/settings.py
@@ -7,20 +7,20 @@ from dotenv import load_dotenv
 class Settings:
     def __init__(self):
         # Détecte le mode à chaque instanciation (au lieu de au moment du fichier)
-        mode = os.getenv("MODE", "local")  # "docker" ou "local"
+        mode = os.getenv("MODE", "local")  # "docker", "local", "aws"
 
         if mode == "docker":
-            load_dotenv(dotenv_path="env_folder/.env.postgre.docker")
+            load_dotenv("env_folder/.env.postgre.docker")
+        elif mode == "aws":
+            load_dotenv("env_folder/.env.postgre.aws")
         else:
-            load_dotenv(dotenv_path="env_folder/.env.postgre.local")
+            load_dotenv("env_folder/.env.postgre.local")
 
-        # Maintenant les variables sont chargées correctement
         self.POSTGRES_DB = self._get_env_var("POSTGRES_DB")
         self.POSTGRES_USER = self._get_env_var("POSTGRES_USER")
         self.POSTGRES_PASSWORD = self._get_env_var("POSTGRES_PASSWORD")
         self.POSTGRES_HOST = self._get_env_var("POSTGRES_HOST")
         self.POSTGRES_PORT = os.getenv("POSTGRES_PORT", "5432")
-        self.DUCKDB_PATH = os.getenv("DUCKDB_PATH")
 
         self.DATABASE_URL = (
             f"postgresql+psycopg2://{self.POSTGRES_USER}:{self.POSTGRES_PASSWORD}"
---
+++ tools/changes1/changes_from_v008_to_v009.md
@@ -0,0 +1,9 @@
+# 🔄 Changes from Version 8 to 9
+_Generated on 2025-04-30 20:37:09_
+
+## ✅ Added
+- `│   └── workspace.xml (13.8 Ko)`
+- `│   ├── main.py (1.5 Ko)`
+- `│   │   │   │   │   ├── index-Byp03uTG.js (312.8 Ko)`
+- `│   │   │   │   │   └── index-ChwZJ8QC.css (7.0 Ko)`
+- `│   │   │   │   │   ├── CreateProfile.tsx (5.0 Ko)`
